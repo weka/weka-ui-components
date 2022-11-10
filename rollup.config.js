12d90c568ee9e1aea9e0cf56404317e03af13368
Tomer Cohen <tomerc@weka.io>
2022-11-10 18:14:24 +0200

    init share

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import external from 'rollup-plugin-peer-deps-external';
+import scss from 'rollup-plugin-scss'
+import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+
+
+export default [
+  {
+    input: './src/index.js',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/index.es.js',
+        format: 'es',
+        exports: 'named',
+      }
+    ],
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+        presets: ['@babel/preset-react']
+      }),
+      commonjs(),
+      external(),
+      scss(),
+      resolve(),
+      terser(),
+    ]
+  }
+];
