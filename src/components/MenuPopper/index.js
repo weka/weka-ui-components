f3eaef6cbb80174e555eaa1fdc8b39536d0b6a57
Tomer Cohen <tomerc@weka.io>
2022-11-15 11:43:01 +0200

    v0.0.4

---
+++ src/components/MenuPopper/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import MenuPopper from './MenuPopper'
+
+const MenuPopperWrapper = (props) => <MenuPopper {...props} />
+
+export default React.memo(MenuPopperWrapper)
---
+++ src/components/Tab/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import Tab from './Tab'
+
+const TabWrapper = (props) => <Tab {...props} />
+
+export default React.memo(TabWrapper)
