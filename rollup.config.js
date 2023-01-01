4116f318b58d2b47f34ccbcf4a7f7188a559e367
Tomer Cohen <tomerc@weka.io>
2023-01-01 16:54:34 +0200

    UI: change to TS

--- rollup.config.js
+++ rollup.config.js
@@ -25,6 +25,7 @@ export default [
         sourcemap: true,
       },
     ],
+    external: ['@mui/material', 'react', 'react-dom'],
     plugins: [
       babel({
         babelHelpers: 'runtime',
