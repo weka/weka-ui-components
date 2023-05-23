7497243e1febf99d38ad62128180cc461cb6b16b
Sofiia Tatarinova <sofiia.tatarinova@weka.io>
2023-05-23 16:17:42 +0300

    UI: update upload field, add switch cell

--- dist/cjs/types/components/Table/PerPage/PerPage.d.ts
+++ dist/cjs/types/components/Table/PerPage/PerPage.d.ts
@@ -1,8 +1,8 @@
-/// <reference types="react" />
+import { Dispatch, SetStateAction } from 'react';
 import './perPage.scss';
 interface PerPageProps {
     value: string | number;
-    onChange: (val: string | number) => void;
+    onChange: ((val: string | number) => void) | Dispatch<SetStateAction<number>>;
     options?: Option[];
 }
 declare function PerPage({ value, onChange, options }: PerPageProps): JSX.Element;
