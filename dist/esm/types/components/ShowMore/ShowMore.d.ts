f3eaa446ccef601454612478f67498a3badad9e5
Sofiia Tatarinova <sofiia.tatarinova@weka.io>
2023-11-21 15:13:23 +0200

    UI: add ShowMore component (for TF Wizard Form)

---
+++ dist/esm/types/components/ShowMore/ShowMore.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+import './showMore.scss';
+declare function ShowMore(props: {
+    isClose: boolean;
+    onClick: () => void;
+}): JSX.Element;
+export default ShowMore;
---
+++ src/components/ShowMore/ShowMore.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import clsx from 'clsx'
+import { IconButton } from '@mui/material'
+
+import { Arrow } from '../../svgs'
+
+import './showMore.scss'
+
+function ShowMore(props: { isClose: boolean; onClick: () => void }) {
+  const { isClose, onClick } = props
+  const showClasses = clsx({ close: isClose })
+  return (
+    <IconButton onClick={onClick} className='show-more-wrapper'>
+      <Arrow className={showClasses} />
+    </IconButton>
+  )
+}
+
+export default ShowMore
