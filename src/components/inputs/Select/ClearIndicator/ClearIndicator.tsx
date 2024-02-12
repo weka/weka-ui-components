1e192135efb1f51d78040c202fad31a9c4ecfdfa
Maxim Voloshin <maxim.voloshin@weka.io>
2024-02-12 19:30:44 +0200

    UI: allow unremovable options in Select

--- src/components/inputs/Select/ClearIndicator/ClearIndicator.tsx
+++ src/components/inputs/Select/ClearIndicator/ClearIndicator.tsx
@@ -4,19 +4,24 @@ import Tooltip from '../../../Tooltip'
 import { EMPTY_STRING } from '../../../../consts'
 
 interface ClearIndicatorProp extends ClearIndicatorProps {
-    selectProps: any
+  selectProps: any
 }
 function ClearIndicator(props: ClearIndicatorProp) {
   const { selectProps } = props
 
+  const isOnlyUnremovableSelected =
+    Array.isArray(selectProps.value) &&
+    selectProps.value.every((option: Option) => option.isUnremovable)
+
   return (
-    <Tooltip data={selectProps.clearText || EMPTY_STRING}>
-      <div>
-        <components.ClearIndicator {...props} />
-      </div>
-    </Tooltip>
+    !isOnlyUnremovableSelected && (
+      <Tooltip data={selectProps.clearText || EMPTY_STRING}>
+        <div>
+          <components.ClearIndicator {...props} />
+        </div>
+      </Tooltip>
+    )
   )
 }
-
 
 export default ClearIndicator
