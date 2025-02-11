03497a630da173a5e0667816246c50dcf4f037b1
Vova Puchkov <volodymyr.puchkov@weka.io>
2025-02-11 14:38:38 +0200

    feat(SeverityCell): accept className prop (#244)

--- lib/components/Table/exports/cells/SeverityCell/SeverityCell.tsx
+++ lib/components/Table/exports/cells/SeverityCell/SeverityCell.tsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import clsx from 'clsx'
 import Tooltip from '../../../../Tooltip'
-import { SEVERITIES_ICONS } from 'consts'
+import { SEVERITIES_ICONS, EMPTY_STRING } from 'consts'
 import { ExtendedCellProps } from '../../../types'
 
 import './severityCell.scss'
 
 export type SeverityCellValue = string
 
-function SeverityCell<Data>(props: ExtendedCellProps<Data, SeverityCellValue>) {
-  const { cell, customValue } = props
+interface SeverityCellProps<Data>
+  extends ExtendedCellProps<Data, SeverityCellValue> {
+  extraClass?: string
+}
+
+function SeverityCell<Data>(props: SeverityCellProps<Data>) {
+  const { cell, customValue, extraClass = EMPTY_STRING } = props
 
   const value = customValue !== undefined ? customValue : cell.getValue()
 
   const Icon = SEVERITIES_ICONS[value]
-  const classes = clsx({
-    'event-severity': true,
-    [value.toLowerCase()]: true
-  })
+  const classes = clsx('event-severity', value.toLowerCase(), extraClass)
   return (
     <Tooltip data={value} placement='right'>
       <div className={classes}>{Icon ? <Icon /> : value}</div>
