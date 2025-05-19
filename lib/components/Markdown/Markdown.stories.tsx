f753ca7abc420a7ce126bdf9ad51db163c8af112
Sofiia Tatarinova <63452816+sofa-tata@users.noreply.github.com>
2025-05-19 16:27:17 +0300

    feat: add Markdown component (#285)

---
+++ lib/components/Markdown/Markdown.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { default as MarkdownComponent } from './Markdown'
+import React from 'react'
+
+const meta: Meta<typeof MarkdownComponent> = {
+  component: MarkdownComponent,
+  title: 'Components/Markdown'
+}
+
+export default meta
+type Story = StoryObj<typeof MarkdownComponent>
+
+export const Default: Story = {
+  args: {
+    children: 'Here is some **bold** text and [a link](https://example.com)'
+  },
+  render: (args: { children: string }) => (
+    <MarkdownComponent {...args}>
+      {args.children}
+    </MarkdownComponent>
+  )
+}
