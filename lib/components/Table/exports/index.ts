291aa752799e98057401e98ac6ae47630f97b1c6
Maxim Voloshin <116369607+mvoloshin0458@users.noreply.github.com>
2026-03-09 11:15:58 +0200

    fix: Fix filters on tables (WH-3758) (#354)

--- lib/components/Table/exports/index.ts
+++ lib/components/Table/exports/index.ts
@@ -3,6 +3,7 @@ export * from './aggregatedCells'
 export { createColumnHelperWithAction as createColumnHelper } from './utils'
 export { default as PerPage } from './PerPage'
 export { useUrlFilters } from '../hooks'
+export { FILTER_MODES } from '../tableUtils'
 
 import type {
   FilterFn,
